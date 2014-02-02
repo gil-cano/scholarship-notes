7f6535382bed5948d7c62710bacfe9d93b7f51d7
Gildardo Bautista <gil.cano@gmail.com>
2014-02-02 16:12:40 -0600

    Agregamos nuevos campos 2014

--- rstbecas.py
+++ rstbecas.py
@@ -2,13 +2,28 @@
 
 import csv
 
-header_label = [u'userid', u'A.1. Nombre', u'A.2. Correo electronico',
-u'A.3. Nombre asesor', u'A.4. Correo electrónico del asesor', u'A.5. Número de cuenta',
-u'A.6. Fecha de nacimiento', u'A.7. Sexo', u'A.8. País de recidencia permanente',
-u'A.9. Nacionalidad', u'A.10. Teléfono particular', u'B.1. Beca', u'B.2. Tipo de beca',
-u'B.3. Solicita cambio de parcial a completa?', u'B.4. Lugar asignado',
-u'B.5. Tiene beca económica?', u'B.6. Que institución otorga la beca económica?',
-u'B.7. Fecha de primer ingreso como becario en el IMATE', u'C.1.1. Nombre de la licenciatura',
+header_label = [
+    u'userid',
+    u'A.1. Nombre',
+    u'A.2. Correo electronico',
+    u'A.3. Nombre asesor',
+    u'A.4. Correo electrónico del asesor',
+    u'A.5. Número de cuenta',
+    u'A.6. Fecha de nacimiento',
+    u'A.7. Sexo',
+    u'A.8. País de recidencia permanente',
+    u'A.9. Nacionalidad',
+    u'A.10. Teléfono particular',
+    u'A.11. Sede donde es becario',
+    u'B.1. Beca',
+    u'B.2. Tipo de beca que tiene actualmente',
+    u'B.3. ¿Solicita cambio de parcial a completa o de completa a parcial?',
+    u'B.4.1  Lugar asignado - Salón',
+    u'B.4.2  Lugar asignado - Escritorio',
+    u'B.5. Tiene beca económica?',
+    u'B.6. Que institución otorga la beca económica?',
+    u'B.7. Fecha de primer ingreso como becario en el IMATE',
+    u'C.1.1. Nombre de la licenciatura',
 u'C.1.2. Escuela', u'C.1.3. Fecha de ingreso a licenciatura',
 u'C.1.4. Fecha de egreso de licenciatura', u'C.1.5. Porcentaje de avance',
 u'C.1.6. Promedio licenciatura', u'C.1.7. Tesis de licenciatura',
@@ -31,12 +46,12 @@ u'F. Estado de la solicitud', u'Posting Date/Time']
 
 sections = {
 1: [(u'A. INFORMACIÓN GENERAL', '-')],
-11: [(u'B. INFORMACIÓN DE LA BECA', '-')],
-18: [(u'C. ESTUDIOS REALIZADOS', '-'), (u'C.1. LICENCIATURA', '~')],
-25: [(u'C.2. MAESTRÍA', '~')],
-33: [(u'C.3. DOCTORADO', '~')],
-40: [(u'D INFORME', '-'), (u'D.1. INFORME DE ACTIVIDADES REALIZADAS', '~')],
-48: [(u'D.2. PLAN DE ACTIVIDADES', '~')],
+12: [(u'B. INFORMACIÓN DE LA BECA', '-')],
+20: [(u'C. ESTUDIOS REALIZADOS', '-'), (u'C.1. LICENCIATURA', '~')],
+27: [(u'C.2. MAESTRÍA', '~')],
+35: [(u'C.3. DOCTORADO', '~')],
+42: [(u'D INFORME', '-'), (u'D.1. INFORME DE ACTIVIDADES REALIZADAS', '~')],
+50: [(u'D.2. PLAN DE ACTIVIDADES', '~')],
 }
 
 csvreader = csv.reader(open('applications.csv', 'r'), delimiter=',')
@@ -72,43 +87,43 @@ for row in rows:
     outfile.write('{\\b %s}' % (name.decode('UTF-8').encode('rtfunicode')))
     outfile.write('\par}\n')
 
-    # Resume
-    resume = [
-        (1, 'NOMBRE'),
-        (41, 'NIVEL'),
-        (3, 'ASESOR'),
-        (12, 'TIPO DE BECA'),
-        (13, '\¿SOLICITA CAMBIO?'),
-        (15, 'BECA ECONOMICA'),        
-    ]
-    for index, label in resume:
-        value = row[1][index].strip()
-        outfile.write('{\pard')
-        outfile.write('{\\b %s: }' % label)
-        outfile.write(value.decode('UTF-8').encode('rtfunicode'))
-        outfile.write('\par}\n')
-    # ingreso
-    nivel = row[1][41].strip()
-    if nivel == 'Licenciatura':
-        ingreso = row[1][20].strip()
-    elif nivel == 'Doctorado':
-        ingreso = row[1][35].strip()
-    else:
-        ingreso = row[1][27].strip()
-    outfile.write('{\pard')
-    outfile.write('{\\b INGRESO: }')
-    outfile.write(ingreso.decode('UTF-8').encode('rtfunicode'))
-    outfile.write('\par}\n')
-    #AVANCE
-    outfile.write('{\pard')
-    outfile.write('{\\b AVANCE: }')
+    # # Resume
+    # resume = [
+    #     (1, 'NOMBRE'),
+    #     (41, 'NIVEL'),
+    #     (3, 'ASESOR'),
+    #     (12, 'TIPO DE BECA'),
+    #     (13, '\¿SOLICITA CAMBIO?'),
+    #     (15, 'BECA ECONOMICA'),
+    # ]
+    # for index, label in resume:
+    #     value = row[1][index].strip()
+    #     outfile.write('{\pard')
+    #     outfile.write('{\\b %s: }' % label)
+    #     outfile.write(value.decode('UTF-8').encode('rtfunicode'))
+    #     outfile.write('\par}\n')
+    # # ingreso
+    # nivel = row[1][41].strip()
+    # if nivel == 'Licenciatura':
+    #     ingreso = row[1][20].strip()
+    # elif nivel == 'Doctorado':
+    #     ingreso = row[1][35].strip()
+    # else:
+    #     ingreso = row[1][27].strip()
+    # outfile.write('{\pard')
+    # outfile.write('{\\b INGRESO: }')
     # outfile.write(ingreso.decode('UTF-8').encode('rtfunicode'))
-    outfile.write('\par}\n')
-    # RECOMENDACION:
-    outfile.write('{\pard')
-    outfile.write('{\\b RECOMENDACION: }')
-    # outfile.write(ingreso.decode('UTF-8').encode('rtfunicode'))
-    outfile.write('\par}\n')
+    # outfile.write('\par}\n')
+    # #AVANCE
+    # outfile.write('{\pard')
+    # outfile.write('{\\b AVANCE: }')
+    # # outfile.write(ingreso.decode('UTF-8').encode('rtfunicode'))
+    # outfile.write('\par}\n')
+    # # RECOMENDACION:
+    # outfile.write('{\pard')
+    # outfile.write('{\\b RECOMENDACION: }')
+    # # outfile.write(ingreso.decode('UTF-8').encode('rtfunicode'))
+    # outfile.write('\par}\n')
 
 
     # write fields
